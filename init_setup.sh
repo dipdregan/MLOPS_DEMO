ee612b4cb5f4786c24dc0b71d9bc0e17571430df
Dipendra Pratap <dipendrapratap155@gmail.comgit config --global user.email dipendrapratap155@gmail.com>
2024-01-08 21:09:58 +0530

    project setup done

--- init_setup.sh
+++ init_setup.sh
@@ -1,1 +1,8 @@
-
+echo "$(date): Start"
+echo "$(date): Creating env with Python 3.8 version"
+conda create --prefix ./env python=3.8 -y
+echo "$(date): Activating the env"
+source ./env/Scripts/activate
+echo "$(date): Installing the requirements"
+pip install -r requirements_dev.txt
+echo "$(date): End"
